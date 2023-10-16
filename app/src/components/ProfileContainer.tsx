7a098b082bd59ed917f3817abb5c58318bf287d5
aldotestino <aldo.testino@libero.it>
2023-10-16 09:26:05 +0200

    fix: scrolling view in profile page

--- app/src/components/ProfileContainer.tsx
+++ app/src/components/ProfileContainer.tsx
@@ -1,8 +1,8 @@
-import { BoxProps, VStack, Icon, Text } from '@chakra-ui/react';
+import { BoxProps, VStack, Icon, Text, Box } from '@chakra-ui/react';
 import ProfileHeader from './ProfileHeader';
 import { InboxIcon } from '@heroicons/react/24/outline';
 
-// const fountains = Array(50).fill(1);
+const fountains = Array(30).fill(1);
 
 function ProfileContainer({ ...boxProps }: BoxProps) {
 
@@ -10,16 +10,15 @@ function ProfileContainer({ ...boxProps }: BoxProps) {
     <VStack px={4} {...boxProps} alignItems="start" w="full" maxW="container.sm" overflowY="hidden">
       <ProfileHeader />
 
-
-      <VStack pt="20%" w="full" alignItems="center">
-        <Icon color="slate.500" as={InboxIcon} w={12} h={12} />
-        <Text textAlign="center" color="slate.500" fontWeight="bold">Non hai ancora aggiunto nessuna fontana</Text>
-      </VStack>
-
-
-      {/* <Box flexGrow={1} w="full" maxH={40} bg="red.500" overflowY="scroll">
-        {fountains.map((_, i) => <p key={i}>{i}</p>)}
-      </Box> */}
+      {fountains.length === 0 ? 
+        <VStack pt="20%" w="full" alignItems="center">
+          <Icon color="slate.500" as={InboxIcon} w={12} h={12} />
+          <Text textAlign="center" color="slate.500" fontWeight="bold">Non hai ancora aggiunto nessuna fontana</Text>
+        </VStack> :
+        <Box flex="1 auto" height="0" w="full" bg="red.500" overflowY="scroll">
+          {fountains.map((_, i) => <p key={i}>{i}</p>)}
+        </Box>
+      }
 
     </VStack>
   );
