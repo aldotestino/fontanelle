59a9fc4a5c8ec66a88f4d0c6b126d855c9292cdb
aldotestino <aldo.testino@libero.it>
2023-10-23 01:35:08 +0200

    update axios base url

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { QueryClient } from 'react-query';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
 });
 
 const queryClient = new QueryClient();
