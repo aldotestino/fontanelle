b6b4fd8f9c0f72baac6a88102e7aa4fe72835772
aldotestino <aldo.testino@libero.it>
2023-10-24 09:27:31 +0200

    postgres init script

--- configure_certificate.sh
+++ configure_certificate.sh
@@ -2,9 +2,7 @@ mkdir nginx &&
 cp app-nossl.conf ./nginx/app.conf &&
 chmod +x init-letsencrypt.sh &&
 docker compose build &&
-./init-letsencrypt.sh &&
+./init-letsencrypt.sh
 
 rm ./nginx/app.conf &&
-cp app-ssl.conf ./nginx/app.conf &&
-./deploy.sh
-
+cp app-ssl.conf ./nginx/app.conf
