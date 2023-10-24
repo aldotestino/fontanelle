1aff9bdde67c3cbd58e135f92a52514e19f4060a
aldotestino <aldo.testino@libero.it>
2023-10-25 00:01:07 +0200

    chnage domain name

--- configure_certificate.sh
+++ configure_certificate.sh
@@ -2,7 +2,8 @@ mkdir nginx &&
 cp app-nossl.conf ./nginx/app.conf &&
 chmod +x init-letsencrypt.sh &&
 docker compose build &&
-./init-letsencrypt.sh
+./init-letsencrypt.sh &&
+docker compose down && 
 
 rm ./nginx/app.conf &&
 cp app-ssl.conf ./nginx/app.conf
